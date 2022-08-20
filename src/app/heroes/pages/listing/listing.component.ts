838364b11c57ea821dab844c73ed6625a4d1a31f
Rolando Guerra <rolandoguerra1991@gmail.com>
2022-08-19 21:13:04 -0400

    obteniendo heroes

--- src/app/heroes/pages/listing/listing.component.ts
+++ src/app/heroes/pages/listing/listing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-listing',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[] = [];
+
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+    this.heroesService.getHeroes()
+      .subscribe(resp => this.heroes = resp);
   }
 
 }
