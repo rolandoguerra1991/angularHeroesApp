537ebbdd18055d4e8443a41ce5ad9926a93d8c6b
Rolando Guerra <rolandoguerra1991@gmail.com>
2022-08-24 22:30:13 -0400

    guards canLoad

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { AuthRoutingModule } from './auth-routing.module';
   imports: [
     CommonModule,
     AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Auth } from '../../../auth/interfaces/auth.interface';
+import { AuthService } from '../../../auth/services/auth.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  get auth(): Auth {
+    return this.authService.auth;
+  }
+
+  constructor(
+    private router:Router,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    this.router.navigate(['auth/login'])
+  }
 }
